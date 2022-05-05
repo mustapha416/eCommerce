88564617c88716a257999d3e0555de6867eb5e83
mustapha416 <mostafa.ifleh@gmail.com>
2022-05-05 11:44:48 +0200

    navbar

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+
+
+}
+)
+
+function hommes(){
+
+    document.getElementById("s3").style.display ="none"
+}
+
+function femmes(){
+
+    document.getElementById("s3").style.display ="flex"
+}
